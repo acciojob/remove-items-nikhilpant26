2a21003109dfc327f20731ee468aa5700b0a2893
Nikhil <nikhilpanthulu26@gmail.com>
2025-07-31 00:41:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const dropdown = document.getElementById("colorSelect")
+function removeList(){
+	const selectedIndex = dropdown.selectedIndex
+	
+	if(selectedIndex>=0){
+		dropdown.remove(selectedIndex)
+	}
+}
